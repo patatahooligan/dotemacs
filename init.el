ade1e2ef2f45a79bce73acc380290a90a48d11db
Chris Perivolaropoulos <darksaga2006@gmail.com>
2012-11-19 16:22:01 +0200

    Yank filenames

--- init.el
+++ init.el
@@ -657,3 +657,18 @@ channels in a tmp buffer."
  )
 
 (put 'set-goal-column 'disabled nil)
+
+
+(defun my-put-file-name-on-clipboard ()
+  "Put the current file name on the clipboard"
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (with-temp-buffer
+        (insert filename)
+        (clipboard-kill-region (point-min) (point-max)))
+      (message (format "Copied '%s'" filename)))))
+
+(global-set-key (kbd "C-x y") 'my-put-file-name-on-clipboard)
